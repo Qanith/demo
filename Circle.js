672be8615a4e296e28cc567271959643aaf5bcf8
sunitr70 <suniroy@deloitte.com>
2023-07-21 15:06:55 +0530

    new change

---
+++ Circle.js
@@ -0,0 +1,13 @@
+/// <reference path = "IShape.ts" /> 
+var Drawing;
+(function (Drawing) {
+    var Circle = /** @class */ (function () {
+        function Circle() {
+        }
+        Circle.prototype.draw = function () {
+            console.log("Circle is drawn");
+        };
+        return Circle;
+    }());
+    Drawing.Circle = Circle;
+})(Drawing || (Drawing = {}));
